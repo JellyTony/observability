450bc50aae368a1af08bb28044a333342e766d51
JellyTony <106534114+JellyTony@users.noreply.github.com>
2025-01-07 22:40:55 +0800

    fix bug (#27)

--- src/Middleware/RequestID.php
+++ src/Middleware/RequestID.php
@@ -3,14 +3,21 @@
 namespace JellyTony\Observability\Middleware;
 
 use Closure;
-use Zipkin\Propagation\Id;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use JellyTony\Observability\Constant\Constant;
+use Zipkin\Propagation\Id;
 
 class RequestID
 {
+    public $excluded_paths = [
+        'listen',
+        'metrics'
+    ];
+
     public function handle(Request $request, Closure $next)
     {
+        $callerService = $request->header('x-md-local-caller_service', 'unknown');
         // 兼容swoole的问题
         if (isset($_SERVER['HTTP_X-REQUEST-ID']) && !empty($_SERVER['HTTP_X-REQUEST-ID'])) {
             $_SERVER[Constant::HTTP_X_REQUEST_ID] = $_SERVER['HTTP_X-REQUEST-ID'];
@@ -24,14 +31,37 @@ class RequestID
 
         // 注入header头 x-request-id
         if (!isset($_SERVER[Constant::HTTP_X_REQUEST_ID]) || empty($_SERVER[Constant::HTTP_X_REQUEST_ID])) {
+            if(!$this->shouldBeExcluded($request->path())) {
+                \Log::debug('need trace request_id request from ' . $callerService, ['global_fields' => [
+                    "caller" => $callerService,
+                    'route' => $request->path(),
+                ]]);
+            }
             $_SERVER[Constant::HTTP_X_REQUEST_ID] = uuidV4();
         }
         // 注入 header 头x-b3-traceid
         if (!isset($_SERVER[Constant::HTTP_X_B3_TRACE_ID]) || empty($_SERVER[Constant::HTTP_X_B3_TRACE_ID])) {
+            if(!$this->shouldBeExcluded($request->path())) {
+                \Log::debug('need trace trace_id request from ' . $callerService, ['global_fields' => [
+                    "caller" => $callerService,
+                    'route' => $request->path(),
+                ]]);
+            }
             $_SERVER[Constant::HTTP_X_B3_TRACE_ID] = Id\generateNextId();
         }
 
         return $next($request);
+    }
+
+    protected function shouldBeExcluded(string $path): bool
+    {
+        foreach ($this->excluded_paths as $excludedPath) {
+            if (Str::is($excludedPath, $path)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
